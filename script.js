700e6ca7f443738ffbb46e4a1c983e56bab5a3d6
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-16 10:36:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.quesrySelector("input[type='button]'").addEventListener("click",()=>{
+	const colorSelect = document.getElementById("colorSelect");
+	if(colorSelect.selectedIndex !== -1){
+		colorSelect.remove(colorSelect.selectedIndex);
+	}
+})
